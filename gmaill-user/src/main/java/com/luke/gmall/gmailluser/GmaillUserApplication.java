7a417a76795e874fad6a02e510628091fc2774ad
Luke <zenglingkun168@gmail.com>
2020-08-17 11:06:44 -0700

    add the user spring module

---
+++ gmaill-user/src/main/java/com/luke/gmall/gmailluser/GmaillUserApplication.java
@@ -0,0 +1,13 @@
+package com.luke.gmall.gmailluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmaillUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmaillUserApplication.class, args);
+	}
+
+}
---
+++ gmaill-user/src/test/java/com/luke/gmall/gmailluser/GmaillUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luke.gmall.gmailluser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmaillUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
